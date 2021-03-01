fad4d72236ff09c174e4c1617aa37c88306d52f2
juanancarrillo <jcjacarrillo@gmail.com>
2021-03-01 10:57:49 +0100

    Show list and loading

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Loading from './components/Loading'
+import React, { Component } from 'react';
+import List from './components/List';
 
-class App extends Component{
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <Loading/>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-
+class App extends Component {
+  render() {
+    return (<List/>);
   }
-  
 }
 
 export default App;
